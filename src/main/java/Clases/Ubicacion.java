c1095e9f8f1f86e326bd82c8cafea06f7654b935
davidheay <deha121212@outlook.es>
2019-11-16 17:13:39 -0500

    initial

---
+++ src/main/java/Clases/Ubicacion.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author deha1
+ */
+public class Ubicacion {
+
+    private String latitud;
+    private String longitud;
+
+    public Ubicacion() {
+    }
+
+    public Ubicacion(String latitud, String longitud) {
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+}
